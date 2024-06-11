4fd965e213f1d4342015a435022b2dcf4c1f4f3f
Irvin Lu <scurvyirv@scurvyirvy.attlocal.net>
2024-06-10 17:00:10 -0700

    this commit updates the prompt questions for better ordering, creates shapes.test.js logic but rendering issues persist with square shape

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 //import shapes
 const { Circle, Square, Triangle } = require('./lib/shapes.js');
@@ -6,16 +7,6 @@ const { Circle, Square, Triangle } = require('./lib/shapes.js');
 //this will prompt questions on the terminal to determine personal logo style
 inquirer
     .prompt([
-        {
-            type: "input",
-            message: "what three characters would you like to use?",
-            name: "text",
-        },
-        {
-            type: "input",
-            message: "what color will your text be?",
-            name: "textColor",
-        },
         {
             type: "list",
             message: "what shape would you like to use?",
@@ -28,35 +19,54 @@ inquirer
         },
         {
             type: "input",
-            message: "what color would you like to use?",
+            message: "what color will your shape be?",
             name: "color"
+        },
+        {
+            type: "input",
+            message: "what three characters would you like to use?",
+            name: "text",
+        },
+        {
+            type: "input",
+            message: "what color will your text be?",
+            name: "textColor",
         }
     ])
     .then((response) => {
         // this is an instantiation of the newLogo based on responses from terminal 
         let newShape;
-            switch (response.shapes) {
-                case "circle":
-                    newShape = new Circle();
-                    break;
-                case "square":
-                    newShape = new Square();
-                    break;
-                case "triangle":
-                    newShape = new Triangle();
-                    break;
+        switch (response.shapes) {
+            case "circle":
+                newShape = new Circle();
+                break;
+            case "square":
+                newShape = new Square();
+                break;
+            case "triangle":
+                newShape = new Triangle();
+                break;
         }
-        // NEED logic for shape conditions (if/else...OR SWITCH when deciding between circle/square/triangle)
-
-        //invoke the methods from shape class
+    
+        // invoke the methods from shape class
         newShape.setText(response.text);
         newShape.setColor(response.color);
         newShape.setTextColor(response.textColor);
-
-        //invoke SVG logic into terminal (BUT REALLY WE NEED TO TRANSCRIBE IT INTO SVG FILE using WRITEFILE)
-        const renderedShape = newShape.render()
+    
+        // invoke SVG logic into terminal
+        const renderedShape = newShape.render();
         console.log(renderedShape);
+    
+        // invoke SVG logic into a text file
+        return fs.writeFile('renderedShape.svg', renderedShape, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('logo file successfully generated!')
+        });
     })
+
     .catch((error) => {
         console.log(error)
     });
